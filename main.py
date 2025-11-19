70a999b6575e3c88ef9d872beaff230fad3e9ed0
niteshpal-25 <niteshpal9619@gmail.com>
2025-11-19 22:58:23 +0530

    Created role CRUD apis

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database.database import engine
 from database import models
-from routers import auth,employee
+from routers import auth,employee,role
 
 app = FastAPI(
     title="Grethr Clone API",
@@ -24,5 +24,6 @@ app.add_middleware(
 
 app.include_router(auth.router)
 app.include_router(employee.router)
+app.include_router(role.router)
 
 models.Base.metadata.create_all(bind=engine)
