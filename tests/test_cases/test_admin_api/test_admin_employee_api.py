09e073a3554f7e472d66d81f054c52d3957ddd45
vishalpal-06 <vishalpal622003@gmail.com>
2025-12-02 16:58:46 +0530

    Added TestCases for regularization

--- tests/test_cases/test_admin_api/test_admin_employee_api.py
+++ tests/test_cases/test_admin_api/test_admin_employee_api.py
@@ -82,7 +82,7 @@ def test_admin_access_manager_subordinate_by_email_not_found(client, admin_user)
 
 
 # -------------------------------------------------test admin api ---------------------------------------------------
-def test_admin_access_admin_get_all_employees_forbidden(client, admin_user):
+def test_admin_access_admin_get_all_employees_success(client, admin_user):
     response = client.get(
         "/admin/employees/",
         headers={"Authorization": f"Bearer {admin_user}"}
@@ -92,7 +92,7 @@ def test_admin_access_admin_get_all_employees_forbidden(client, admin_user):
     assert response.json() == expected
 
 
-def test_admin_access_admin_create_employee_forbidden(client, admin_user):
+def test_admin_access_admin_create_employee_success(client, admin_user):
     payload = {
         "first_name": "string",
         "last_name": "string",
@@ -117,88 +117,166 @@ def test_admin_access_admin_create_employee_forbidden(client, admin_user):
     assert response.json() == expected
 
 
-# def test_admin_access_admin_get_employee_by_id_forbidden(client, admin_user):
-#     response = client.get(
-#         "/admin/employees/id/1",
-#         headers={"Authorization": f"Bearer {admin_user}"}
-#     )
-
-#     assert response.status_code == 403
-#     assert response.json() == {"detail": "Admin privileges required"}
-
-
-# def test_admin_access_admin_update_employee_by_id_forbidden(client, admin_user):
-#     payload = {
-#         "first_name": "string",
-#         "last_name": "string",
-#         "joining_date": "2025-11-27",
-#         "address": "string",
-#         "isadmin": True,
-#         "fk_department_id": 1,
-#         "fk_role_id": 1,
-#         "fk_manager_id": 1,
-#         "password": "string"
-#     }
-
-#     response = client.put(
-#         "/admin/employees/id/1",
-#         json=payload,
-#         headers={"Authorization": f"Bearer {admin_user}"}
-#     )
-
-#     assert response.status_code == 403
-#     assert response.json() == {"detail": "Admin privileges required"}
+def test_admin_access_admin_get_employee_by_id_success(client, admin_user):
+    response = client.get(
+        "/admin/employees/id/1",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    expected = read_json("get_employee_details_by_id.json")
+    assert response.status_code == 200
+    assert response.json() == expected
 
 
-# def test_admin_access_admin_delete_employee_by_id_forbidden(client, admin_user):
-#     response = client.delete(
-#         "/admin/employees/id/1",
-#         headers={"Authorization": f"Bearer {admin_user}"}
-#     )
-
-#     assert response.status_code == 403
-#     assert response.json() == {"detail": "Admin privileges required"}
-
-
-# def test_admin_access_admin_get_employee_by_email_forbidden(client, admin_user):
-#     response = client.get(
-#         "/admin/employees/email/admin@test.com",
-#         headers={"Authorization": f"Bearer {admin_user}"}
-#     )
-
-#     assert response.status_code == 403
-#     assert response.json() == {"detail": "Admin privileges required"}
+def test_admin_access_admin_get_employee_by_id_not_found(client, admin_user):
+    response = client.get(
+        "/admin/employees/id/10",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found"}
 
 
-# def test_admin_access_admin_update_employee_by_email_forbidden(client, admin_user):
-#     payload = {
-#         "first_name": "string",
-#         "last_name": "string",
-#         "joining_date": "2025-11-27",
-#         "address": "string",
-#         "isadmin": True,
-#         "fk_department_id": 1,
-#         "fk_role_id": 1,
-#         "fk_manager_id": 1,
-#         "password": "string"
-#     }
+def test_admin_access_admin_update_employee_by_id_success(client, admin_user):
+    payload = {
+        "first_name": "string",
+        "last_name": "string",
+        "joining_date": "2025-11-27",
+        "address": "string",
+        "isadmin": True,
+        "fk_department_id": 1,
+        "fk_role_id": 1,
+    }
 
-#     response = client.put(
-#         "/admin/employees/email/admin@test.com",
-#         json=payload,
-#         headers={"Authorization": f"Bearer {admin_user}"}
-#     )
-
-#     assert response.status_code == 403
-#     assert response.json() == {"detail": "Admin privileges required"}
+    response = client.put(
+        "/admin/employees/id/4",
+        json=payload,
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    expected = read_json("update_employee_details_by_id.json")
+    assert response.status_code == 200
+    assert response.json() == expected
 
 
-# def test_admin_access_admin_delete_employee_by_email_forbidden(client, admin_user):
-#     response = client.delete(
-#         "/admin/employees/email/admin@test.com",
-#         headers={"Authorization": f"Bearer {admin_user}"}
-#     )
+def test_admin_access_admin_update_employee_by_id_not_found(client, admin_user):
+    payload = {
+        "first_name": "string",
+        "last_name": "string",
+        "joining_date": "2025-11-27",
+        "address": "string",
+        "isadmin": True,
+        "fk_department_id": 1,
+        "fk_role_id": 1,
+        "fk_manager_id": 1,
+        "password": "string"
+    }
 
-#     assert response.status_code == 403
-#     assert response.json() == {"detail": "Admin privileges required"}
+    response = client.put(
+        "/admin/employees/id/14",
+        json=payload,
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found"}
+
+
+def test_admin_access_admin_delete_employee_by_id_success(client, admin_user):
+    response = client.delete(
+        "/admin/employees/id/3",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {"detail": "Employee deleted successfully"}
+
+
+def test_admin_access_admin_delete_employee_by_id_not_found(client, admin_user):
+    response = client.delete(
+        "/admin/employees/id/13",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found"}
+
+
+def test_admin_access_admin_get_employee_by_email_success(client, admin_user):
+    response = client.get(
+        "/admin/employees/email/admin@test.com",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    expected = read_json("get_employee_details_by_email.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_admin_get_employee_by_email_not_found(client, admin_user):
+    response = client.get(
+        "/admin/employees/email/admin2@test.com",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found"}
+
+
+def test_admin_access_admin_update_employee_by_email_success(client, admin_user):
+    payload = {
+        "first_name": "string",
+        "last_name": "string",
+        "joining_date": "2025-11-27",
+        "address": "string",
+        "isadmin": True,
+        "fk_department_id": 1,
+        "fk_role_id": 1,
+    }
+
+    response = client.put(
+        "/admin/employees/email/userA1@test.com",
+        json=payload,
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    expected = read_json("update_employee_details_by_email.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_admin_update_employee_by_email_not_found(client, admin_user):
+    payload = {
+        "first_name": "string",
+        "last_name": "string",
+        "joining_date": "2025-11-27",
+        "address": "string",
+        "isadmin": True,
+        "fk_department_id": 1,
+        "fk_role_id": 1,
+    }
+
+    response = client.put(
+        "/admin/employees/email/userA3@test.com",
+        json=payload,
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found"}
+
+
+def test_admin_access_admin_delete_employee_by_email_success(client, admin_user):
+    response = client.delete(
+        "/admin/employees/email/userA1@test.com",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {"detail": "Employee deleted successfully"}
+
+
+def test_admin_access_admin_delete_employee_by_email_not_found(client, admin_user):
+    response = client.delete(
+        "/admin/employees/email/user_A5@test.com",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found"}
 
---
+++ tests/test_cases/test_admin_api/test_admin_regularization_api.py
@@ -0,0 +1,146 @@
+import json
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent.parent.parent / "expected_responses/admin/regularization/"
+
+def read_json(filename):
+    with open(BASE_PATH / filename, "r") as f:
+        return json.load(f)
+
+
+# -------------------------------------------------test manager api ---------------------------------------------------
+def test_admin_access_manager_get_subordinate_regularization_by_status_success(client, admin_user):
+    response = client.get(
+        "/manager/regularizations/pending",
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    expected = read_json("get_subordinate_regularization_by_status_admin.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_manager_get_subordinate_regularization_by_id_success(client, admin_user):
+    response = client.get(
+        "manager/regularizations/1",
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    expected = read_json("get_subordinate_regularization_by_id_admin.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_manager_get_subordinate_regularization_by_id_forbidden(client, admin_user):
+    response = client.get(
+        "manager/regularizations/4",
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Regularization not found under your management"}
+
+
+def test_admin_access_manager_get_subordinate_regularization_by_id_not_found(client, admin_user):
+    response = client.get(
+        "manager/regularizations/15",
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Regularization request not found"}
+
+
+def test_admin_access_manager_get_subordinate_regularization_by_empid_success(client, admin_user):
+    response = client.get(
+        "manager/regularizations/employee/2",
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    expected = read_json("get_subordinate_regularization_by_empid_admin.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_manager_get_subordinate_regularization_by_empid_forbidden(client, admin_user):
+    response = client.get(
+        "manager/regularizations/employee/1",
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found under your management"}
+
+
+def test_admin_access_manager_get_subordinate_regularization_by_empid_not_found(client, admin_user):
+    response = client.get(
+        "manager/regularizations/employee/10",
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Employee not found"}
+
+
+def test_admin_access_manager_update_regularization_status_by_id_success(client, admin_user):
+    response = client.put(
+        "/manager/regularizations/1/status",
+        json={"regularization_status":"Rejected"},
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    expected = read_json("update_subordinate_regularization_status_admin.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_manager_update_regularization_status_by_id_forbidden(client, admin_user):
+    response = client.put(
+        "/manager/regularizations/6/status",
+        json={"regularization_status":"Rejected"},
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Regularization not found under your management"}
+
+
+def test_admin_access_manager_update_regularization_status_by_id_not_found(client, admin_user):
+    response = client.put(
+        "/manager/regularizations/16/status",
+        json={"regularization_status":"Rejected"},
+        headers={"Authorization": f"Bearer {admin_user}"},
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Regularization request not found"}
+
+# -------------------------------------------------test admin api ---------------------------------------------------
+def test_admin_access_admin_get_regularization_by_id_success(client, admin_user):
+    response = client.get(
+        "/admin/regularizations/1",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    expected = read_json("get_employee_regularization_by_id_admin.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_admin_get_regularization_by_id_not_found(client, admin_user):
+    response = client.get(
+        "/admin/regularizations/100",
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Regularization request not found"}
+
+
+def test_admin_access_admin_update_regularization_by_id_success(client, admin_user):
+    response = client.put(
+        "/admin/regularizations/1/status",
+        json = {"regularization_status": "Rejected"},
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    expected = read_json("update_employee_regularization_by_id_admin.json")
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+def test_admin_access_admin_update_regularization_by_id_not_found(client, admin_user):
+    response = client.put(
+        "/admin/regularizations/100/status",
+        json = {"regularization_status": "Rejected"},
+        headers={"Authorization": f"Bearer {admin_user}"}
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Regularization request not found"}
