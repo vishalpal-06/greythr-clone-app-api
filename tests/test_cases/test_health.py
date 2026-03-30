39c56d6c73fdf59193328bea6ebb6c993ff315ef
vishalpal-06 <vishalpal622003@gmail.com>
2026-03-30 13:55:14 +0530

    Updated health message 3

--- tests/test_cases/test_health.py
+++ tests/test_cases/test_health.py
@@ -3,6 +3,4 @@ import json
 
 def test_health(client):
     response = client.get("/")
-    assert response.json() == {
-        "status": "Welcome to My Grehthrapp"
-    }
+    assert response.json() == {"status": "Welcome to My Grehthrapp"}
